c043fad9e6b99d7b4fcf718ed249849e2da8cf51
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-16 23:31:17 +0100

    Add files via upload

---
+++ Logarithm.java
@@ -0,0 +1,11 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Logarithm {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int scan = scanner.nextInt();
+        System.out.println(Math.log(scan));          // 10 * x = scan
+    }
+}
